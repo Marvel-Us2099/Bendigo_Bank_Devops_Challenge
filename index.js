d5e2f2f6b22e02e740a174a6029e2a31a6ee9909
Darcy Wilson <darcen8or@gmail.com>
2021-08-13 12:34:03 +1000

    Addition of latest git commit hash

--- index.js
+++ index.js
@@ -11,14 +11,24 @@
 var http = require('http');
 var dt = require('./module1');
 
+
 http.createServer(function (req, res) {
+    
+    //
+    var pjson = require('./package.json');
+    var revision = require('child_process').execSync('git rev-parse HEAD').toString().trim();
+    
+    //Titles to be bolded for easier reading when displayed to the user.
     var titleName = "Application Name: ";
     var titleVersion = "Version: ";
-    var titleDateTime = "Current date and time: ";
-    var pjson = require('./package.json');
+    var titleGitHash = "Latest Git Commit: "
+    var titleDateTime = "Current Date & Time: ";
 
+// Responces. 
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(titleName.bold() + pjson.name + " <br /> " + titleVersion.bold() + pjson.version + "<br />")
+  res.write(titleName.bold() + pjson.name + " <br /> ");
+  res.write(titleVersion.bold() + pjson.version + "<br />");
+  res.write(titleGitHash.bold() + revision + "<br />");
   res.write(titleDateTime.bold() + dt.myDateTime());
   res.end();
 }).listen(8080);
