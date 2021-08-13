7711d0faa295973bac0f06186242715daebfe0cf
Darcy Wilson <darcen8or@gmail.com>
2021-08-13 22:50:21 +1000

    add git.ignore file - display cleanup

--- README.md
+++
@@ -1,3 +0,0 @@
-# Bendigo_Bank_Devops_Challenge
-Associate DevOps Engineer – Tech Challenge
-Yo you getting this?
--- index.js
+++ index.js
@@ -1,35 +1,32 @@
-
-// Add documentation...
-
-//Developed using wsl (Windows Subsystem for Linux)
-
-// TO run application:
+// To run the application:
+// open powershell
 // Change the working directory to "~/Bendigo_Bank_Devops_Challenge/"
-// run "nodemon nun startAPI" in the terminal for automatic server restart 
+// run "npm install"
+// run "npm init" 
+// run "nodemon run startAPI" in the terminal for automatic server restart 
 
-
-////Display time from module "module1.js"
 var http = require('http');
+//"module1.js" is used for Display module 
 var dt = require('./module1');
 
-
 http.createServer(function (req, res) {
-    
-    //
     var pjson = require('./package.json');
     var revision = require('child_process').execSync('git rev-parse HEAD').toString().trim();
     
     //Titles to be bolded for easier reading when displayed to the user.
     var titleName = "Application Name: ";
     var titleVersion = "Version: ";
-    var titleGitHash = "Latest Git Commit: "
+    var titleGitHash = "Latest Git Commit Hash: "
     var titleDateTime = "Current Date & Time: ";
+    var titleProject = "AFL Stat API:<br />";
 
-
-
+// Responces. 
+  res.writeHead(200, {'Content-Type': 'text/html'}); //Is not required, but what is meant by 'display status 200'
   res.write(titleName.bold() + pjson.name + " <br /> ");
   res.write(titleVersion.bold() + pjson.version + "<br />");
   res.write(titleGitHash.bold() + revision + "<br />");
   res.write(titleDateTime.bold() + dt.myDateTime());
+  res.write("<br /><br /><br />" + titleProject.bold().fontsize(6));
+
   res.end();
 }).listen(8080);
